fa4e2ca82cc27c73671d8f08de02a64010ae0fdb
guillermo.uscudum <guillermo.uscudum@utec.edu.uy>
2023-08-23 12:54:22 -0300

    first commit

---
+++ main.js
@@ -0,0 +1,5 @@
+const URL = "https://fakestoreapi.com/products";
+
+document.addEventListener("DOMContentLoaded", () => {
+    // Fetch a URL y organizamos la info obtenida del JSON en un listado
+});
